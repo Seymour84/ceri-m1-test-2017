dd7d0545ff72028ebd636de596128e16404be0d2
Hueber Dimitri <uapv1302596@ecv-2w26-105.univ-avignon.fr>
2018-03-14 18:14:45 +0100

    debut tp3

---
+++ src/test/java/fr/univavignon/rodeo/IAnimalTest.java
@@ -0,0 +1,43 @@
+package fr.univavignon.rodeo.api;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class IAnimalTest {
+	
+	@Mock
+	private IAnimal animal;
+	
+	@Before
+	public void init() {
+		animal = mock(IAnimal.class);
+		when(animal.getXP()).thenReturn(0);
+		when(animal.isSecret()).thenReturn(true);
+		when(animal.isEndangered()).thenReturn(true);
+		when(animal.isBoss()).thenReturn(true);
+	}
+	
+	@Test
+	public void testGetXp() {
+		assertEquals(0, animal.getXP());
+	}
+	
+	@Test
+	public void testIsSecret() {
+		assertTrue(animal.isSecret());
+	}
+	
+	@Test
+	public void testIsEndangered() {
+		assertTrue(animal.isEndangered());
+	}
+	
+	@Test
+	public void testIsBoss() {
+		assertTrue(animal.isBoss());
+	}
+}
